fedd5366abbfe87890ca419200e4ce9428255bb4
Julio Freitas <julio.cesar.freitas@everis.com>
2021-03-29 15:37:27 -0300

    Criando Interface para chamada da API e Implementando gradle

---
+++ app/src/main/java/com/example/appfilmes/Data/Api/FilmesDB.kt
@@ -0,0 +1,11 @@
+package com.example.appfilmes.Data.Api
+
+
+interface FilmesDB {
+//    https://api.themoviedb.org/3/movie/550?api_key=5e1ae4c60a5ba26389109bd40111a3e6&page=1
+//    https://api.themoviedb.org/3/movie/550?api_key=5e1ae4c60a5ba26389109bd40111a3e6
+//    https://api.themoviedb.org/3/
+    
+    @GET()
+    
+}
